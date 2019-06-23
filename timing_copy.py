843b48100211dc7c0a5e64556f1904c3949da7e7
YuhtaH <yuhta.honda@gmail.com>
2019-06-23 20:52:42 +0900

    cyclic copy complete

--- timing_copy.py
+++ timing_copy.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python
 import sys
-import csv
 import time
 import threading
 import argparse
+import pandas as pd
+import os.path
+import shutil
 
-def worker(list_file, source_dir, dest_dir, count):
-
-    print("list_file = ",list_file)
-    print("source_dir = ",source_dir)
-    print("dest_dir = ",dest_dir)
-    print("count = ",count)
+def worker(file_list, source_dir, dest_dir, p_cycle_num):
+    for file_item in file_list:
+        source_path = os.path.join(source_dir, file_item)
+        dest_path = os.path.join(dest_dir, file_item)
+        shutil.copyfile(source_path, dest_path)
     time.sleep(8)
 
 def scheduler(interval, list_file, source_dir, dest_dir, f, wait = True):
     base_time = time.time()
     next_time = 0
-    count = 0
+    v_cycle_num = 0
+    df = pd.read_csv(list_file)
+    max_cycle = df.cycle_num.max()
     while True:
-        count += 1
-        t = threading.Thread(target=f, args=([list_file, source_dir, dest_dir, count]))
+        v_cycle_num += 1
+        if v_cycle_num > max_cycle:
+            break
+        new_df = df[df.cycle_num == v_cycle_num]
+        if len(new_df) == 0:
+            continue
+        t = threading.Thread(target=f, args=([new_df.file_name, source_dir, dest_dir, v_cycle_num]))
         t.start()
         if wait:
             t.join()
         next_time = ((base_time - time.time()) % interval) or interval
         time.sleep(next_time)
+    
+    print("End.")
+    quit()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
